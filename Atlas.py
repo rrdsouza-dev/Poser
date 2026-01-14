8da1eba278e077e846bdd624196e41356aac1637
rrdsouza-dev <rennator.souzagmail.com>
2026-01-14 10:45:57 -0300

    Pegamos os dados do beta hihiih

--- Atlas.py
+++ Atlas.py
@@ -1,15 +1,15 @@
-def primeiro_comit():
-    print("Primeiro commit realizado com sucesso!")
-    return
+from webbrowser import get
 
+
+def primeiro_comit(self):
+    input("Giga um numero ai vey: ")
+    
+    regener = '67'
+    while True:
+     if regener == True:
+         get.data
+         print("Pegamos seus dados bb hihihih")
+         
+         
+    
 primeiro_comit()
-
-def link_para_o_repositório():
-    print("Link para repositório.")
-    
-    
-    return "Nossa crutch"
-
-print("Que pae")
-
-link_para_o_repositório()
