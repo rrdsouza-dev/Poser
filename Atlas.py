7a3ff03d3b7a28227ba4d19e817e2d5ba55bb3e6
rrdsouza-dev <rennator.souza@gmail.com>
2026-01-17 05:03:42 -0300

    Código base para treino de função

--- Atlas.py
+++ Atlas.py
@@ -1,10 +1,30 @@
-def veri_idade(idade):
- return idade >= 18
 
-idade = int(input("Qual sua idade?: "))
+#Código base para treino de função
 
-if veri_idade(idade):
-    print("Maior de idade")
+def verificação_de_entrada(idade, permissao_especial, cadastro_ativo, tentou_burlar):
     
+    if tentou_burlar == True:
+        return False
+
+    if not cadastro_ativo:
+     return False   
+ 
+    if idade >= 18 or permissao_especial:
+      return True
+  
+    return False
+
+idade = int(input("Quantos anos tem? "))
+    
+permissao = input("Vc tem permissão especial? ").lower().strip() == "s"
+    
+cadastro = input("Tem castro ativo? ").lower().strip() == "s"
+    
+burlar = input("Tentou Burlar?") == "s"
+    
+if verificação_de_entrada (idade, permissao, cadastro, burlar):
+        print("Pode entrar")
+        
 else:
-    print("Menor de idade")
+     print("Acesso negado")
+        
